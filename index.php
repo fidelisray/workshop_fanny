ef790d4eeb61a90466442bb2de84a1e4ef7853f8
Fidelis Raymond <arjefidelisraymond@gmail.com>
2022-04-24 17:06:21 +0700

    Commit 2 - Menambahkan halaman selamat datang, login pembeli, dan login user

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Selamat Datang</title>
+  </head>
+  <body class="halaman-awal">
+    <section class="content-wrapper">
+      <div class="header">
+        <h1>Selamat datang di Valenty Online Shop</h1>
+        <p>Anda ingin login sebagai apa?</p>
+      </div>
+      <div class="content">
+        <div class="penjual">
+          <h2>Penjual</h2>
+          <div class="gambar">
+            <!-- <h1>foto</h1> -->
+            <img src="./image/penjual.svg" alt="Foto Penjual">
+          </div>
+          <a class="btn btn-primary btn-penjual" href="./admin/login.php" role="button">Login sebagai penjual</a>
+        </div>
+        <div class="pembeli">
+          <h2>Pembeli</h2>
+          <div class="gambar">
+            <!-- <h1>foto</h1> -->
+            <img src="./image/pembeli.svg" alt="Foto Pembeli">
+          </div>
+          <a class="btn btn-primary btn-pembeli" href="./user/login.php" role="button">Login sebagai pembeli</a>
+        </div>
+      </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ user/login.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+    <title>Login</title>
+  </head>
+  <body class="login">
+    <section class="login-wrapper">
+      <div class="foto"></div>
+      <div class="form-login-wrapper">
+        <h3>Selamat Datang</h3>
+        <form class="form-login" method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan email" name="email">
+            <small id="emailHelp" class="form-text text-muted">Silahkan masukkan email anda yang telah terdaftar..</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Password" name="password">
+          </div>
+          <button type="submit" class="btn btn-success btn-login">Masuk</button>
+        </form>
+        <p>Belum punya akun? <a href="#">Buat Akun</a></p>
+      </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
